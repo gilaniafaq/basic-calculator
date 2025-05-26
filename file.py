549851c53d9e2f7397a50733dd7ef718bfd7f1c7
Syed Afaq Gilani <afaq.gilani@gmail.com>
2025-05-26 10:49:39 +0530

    Added new line

--- file.py
+++ file.py
@@ -3,3 +3,5 @@ print("This is the repo for Calculator. It will be updated every 7 days with new
 print("This repo is created on local machine.")
 print("This repo is pushed to git using command line.")
 
+print("We will create a power function in this branch")
+
