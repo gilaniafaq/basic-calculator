34f85d110c57891050a469808a08acc08d3d4b21
Syed Afaq Gilani <afaq.gilani@gmail.com>
2025-05-26 09:59:42 +0530

    Add initial files

---
+++ file.py
@@ -0,0 +1,5 @@
+print("Hello World")
+print("This is the repo for Calculator. It will be updated every 7 days with new features")
+print("This repo is created on local machine.")
+print("This repo is pushed to git using command line.")
+
